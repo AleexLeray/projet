ed4347808254e586e7eb2aa877ff9ca516ab3e3c
AleexLeray <AleexLeray@gmail.com>
2016-06-02 16:07:57 +0200

    new roots

--- server.js
+++ server.js
@@ -1,1 +1,42 @@
-console.log('recoucou');
+// var http = require('http'),
+//  fs = require('fs');
+//
+// http.createServer(function(req, res) {
+//
+//   res.writeHead(200, {
+//     'Content-Type': 'text/html',
+//     'Access-Control-Allow-Origin' : '*'
+// });
+//
+// var readStream = fs.createReadStream(__dirname + '/index.html');
+//
+//
+//  readStream.pipe(res);
+// }).listen(1337);
+//
+// console.log('Visit me at http://localhost:1337');
+
+var express = require('express'),
+ app  = express(),
+ path = require('path');
+
+ app.get('/', function(req, res) {
+ res.sendFile(path.join(__dirname + '/index.html'));
+ });
+
+ app.listen(1337);
+
+var adminRouter = express.Router();
+  adminRouter.get('/admin', function(req, res) {
+    res.send('I am the dashboard!');
+  });
+  adminRouter.get('/users', function(req, res) {
+    res.send('I show all the users!');
+  });
+  adminRouter.get('/posts', function(req, res) {
+    res.send('I show all the posts!');
+  });
+  
+  app.use('/admin', adminRouter);
+
+ console.log('1337 is the magic port!');
